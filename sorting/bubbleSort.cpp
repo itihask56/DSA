024d5421514a952ab2fb6901a60edfd44c2a3dd7
Itihas Verma <103826009+itihask56@users.noreply.github.com>
2025-05-19 09:36:46 +0000

    sorting revised

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,18 +1,50 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int arr[5]={6,2,8,4,10};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    for(int i=1;i<n;i++){
-        for(int j=0;j<n-1;j++){
+// int main(){
+//     int arr[5]={6,2,8,4,10};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     for(int i=1;i<n;i++){
+//         for(int j=0;j<n-1;j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//             }
+//         }
+//     }
+//     cout<<"Sorted array: "<<endl;
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+
+void bubbleSort(int arr[],int size){
+    for(int i=size-2;i>=0;i--){
+        bool swapped = 0;
+        for(int j=0;j<=i;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                bool swapped = 1;
             }
         }
+        if(swapped == 0){
+            break;
+        }
     }
-    cout<<"Sorted array: "<<endl;
-    for(int i=0;i<n;i++){
+}
+
+void printArray(int arr[],int size){
+    for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
+}
+
+int main(){
+    int arr[]={6,2,8,4,10};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Array before sorting: "<<endl;
+    printArray(arr,size);
+    cout<<"\nArray after sorting: "<<endl;
+    bubbleSort(arr,size);
+    printArray(arr,size);
 }
