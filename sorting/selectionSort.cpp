3e8bf5f6e9e34916a2a5a8c9ac2cd20b893e131a
Itihas Verma <103826009+itihask56@users.noreply.github.com>
2025-06-02 17:32:56 +0000

    selection sort done in reverse array

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -40,12 +40,53 @@ void printArray(int arr[],int size){
     }
 }
 
+void printCharArray(char arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+void selectionSortReverse(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int max_index = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]>arr[max_index]){
+                max_index = j;
+            }
+        }
+        swap(arr[i],arr[max_index]);
+    }
+}
+
+void charSelectionSort(char arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_index]){
+                min_index = j;
+            }
+        }
+        swap(arr[i],arr[min_index]);
+    }
+}
 int main(){
-    int arr[]={2,8,4,10,5,6};
+    // int arr[]={2,8,4,10,5,6};
+    // int size = sizeof(arr)/sizeof(arr[0]);
+    // cout<<"Array before sorting: "<<endl;
+    // printArray(arr,size);
+    // // selectionSort(arr,size);
+    // selectionSortReverse(arr,size);
+    // cout<<"\nArray after sorting: "<<endl;
+    // printArray(arr,size);
+
+
+
+    char arr[]={'i','t','i','h','a','s'};
     int size = sizeof(arr)/sizeof(arr[0]);
-    cout<<"Array before sorting: "<<endl;
-    printArray(arr,size);
-    selectionSort(arr,size);
-    cout<<"\nArray after sorting: "<<endl;
-    printArray(arr,size);
+    cout<<"Char Array before Sorting: "<<endl;
+    printCharArray(arr,size);
+    cout<<"\n Char Array after Sorting: "<<endl;
+    charSelectionSort(arr,size);
+    printCharArray(arr,size);
 }
